d199c2af9c8e3c5261cc3e0e055ff4ac053253f5
Marco Ferrari <ferrari.marco@gmail.com>
2017-02-02 12:42:11 +0100

    Implemented InSpec control

--- test/integration/default/inspec/ansible-role-chruby.rb
+++ test/integration/default/inspec/ansible-role-chruby.rb
@@ -1,4 +1,41 @@
 control "ansible-role-chruby" do
   title "ansible-role-chruby role check"
 
+  config = YAML.load_file('defaults/main.yml')
+  version = config['chruby_version']
+
+  describe package('make') do
+    it { should be_installed }
+  end
+
+  describe directory('/usr/local/share/doc/chruby-' + version) do
+      it { should exist }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_readable.by_user('root') }
+  end
+
+  describe file('/etc/profile.d/chruby.sh') do
+      it { should exist }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_readable.by_user('root') }
+      its('mode') { should cmp '0644' }
+  end
+
+  describe file('/usr/local/share/chruby/chruby.sh') do
+      it { should exist }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_readable.by_user('root') }
+      its('mode') { should cmp '0644' }
+  end
+
+  describe file('/usr/local/share/chruby/auto.sh') do
+      it { should exist }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_readable.by_user('root') }
+      its('mode') { should cmp '0644' }
+  end
 end
